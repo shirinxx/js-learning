4a548b999bde8b40921416e9c8a2b2b04a0127b1
Shirin Shukurov <mr.shukurov.shirin@gmail.com>
2023-06-28 03:30:57 +0400

    Done until Basic operators lecture

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,42 @@
+// Values adn Variables
+// let country = 'AZE';
+// let continent = 'Asia';
+let population = 10;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+
+// Data Types
+// let isIsland = false;
+// let language;
+
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+
+
+// let, const and var
+language = 'azerbaijani';
+const country = 'Azerbaijan';
+const continent = 'Asia';
+const isIsland = false;
+// isIsland = true;
+
+
+// Basic Operators
+let halfPopulation = population / 2;
+console.log('In each half ' + halfPopulation);
+let newPopulation = population++;
+console.log(newPopulation);
+
+let finlandPopulation = 6;
+console.log('Finland has more population than Azerbaijan is', finlandPopulation > population);
+
+let averagePop = 33;
+console.log(population > averagePop);
+
+const description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
+console.log(description);
