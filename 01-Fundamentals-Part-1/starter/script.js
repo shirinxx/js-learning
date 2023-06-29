af23965cce93289795830d9d8bdf3cd32acf64e0
Shirin Shukurov <mr.shukurov.shirin@gmail.com>
2023-06-29 23:51:27 +0400

    string and template literals

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,13 @@
 let js = "amazing";
 // console.log(10 + 8 -56);
+
+const firstName = 'John';
+const job = 'developer';
+const currentYear = 2023;
+const birthYear = 1999;
+
+const fullDesc = firstName + ' is a ' + job + ' and he is ' + (currentYear - birthYear) + ' years old.';
+console.log(fullDesc);
+
+const testTempplate = `${firstName} is a ${job} and he is ${currentYear - birthYear} years old.`;
+console.log(testTempplate);
