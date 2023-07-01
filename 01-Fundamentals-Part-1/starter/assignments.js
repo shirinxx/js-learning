5078f74038cb74a52d928f5e127696b8eac6c946
Shirin Shukurov <mr.shukurov.shirin@gmail.com>
2023-07-01 23:36:32 +0400

    Taking Decisions: if / else Statements done

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -40,3 +40,16 @@ console.log(population > averagePop);
 
 const description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
 console.log(description);
+
+
+//Strings and Template Literals
+const description2 = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+console.log(description2)
+
+
+//Taking Decisions: if / else Statements
+if(population > 33) {
+    console.log(`${country}'s population is above average`);
+}else{
+    console.log(`${country}'s population is ${33 - population} million below average`);
+}
