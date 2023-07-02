08ea9731062b7111c536623fe4efd602bb99b5ae
Shirin Shukurov <mr.shukurov.shirin@gmail.com>
2023-07-02 17:58:47 +0400

    Logical Operatorss done

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -64,14 +64,22 @@ console.log('123' < 57); // predict false
 console.log(arg1); // predict 1143 typeof number
 
 // Equality Operators: == vs. ===
-const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+// const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
 
-if( numNeighbours === 1 ){
-    console.log('Only 1 border!');
-}
-else if( numNeighbours > 1 ){
-    console.log('More than 1 border');
+// if( numNeighbours === 1 ){
+//     console.log('Only 1 border!');
+// }
+// else if( numNeighbours > 1 ){
+//     console.log('More than 1 border');
+// }
+// else{
+//     console.log('No borders');
+// }
+
+// Logical Operators
+if ( language === 'english' && population < 50 && !isIsland){
+    console.log(`You should live in ${country}`);
 }
 else{
-    console.log('No borders');
+    console.log(`${country} does not meet your criteria :(`);
 }
