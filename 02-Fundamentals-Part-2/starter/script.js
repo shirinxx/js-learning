19be9ca60ef32c520ff39a3217ef13a0e6e0b46c
shirinxx <mr.shukurov.shirin@gmail.com>
2023-07-04 11:01:16 -0500

    assignments.js added

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// Function declarations
+const currentYear = 2023;
+
+function calcAge(birthYear){
+    return currentYear - birthYear;
+}
+console.log(calcAge(1999));
+
+
+// Function Expression
+const calcAge2 = function (birthYear){
+    return currentYear - birthYear;
+}
+
+console.log(calcAge2(1999));
+
+
+//Arrow function
+const calcAge3 = birthYear => currentYear - birthYear;
+console.log(calcAge3(1999));
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = currentYear - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} will retire in ${retirement} years`;
+}
+console.log(yearsUntilRetirement(1999, 'Shirin'));
