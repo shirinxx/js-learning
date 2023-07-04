ea4d5d2122ba2de8a33ceba9dc0b36ebb8926789
shirinxx <mr.shukurov.shirin@gmail.com>
2023-07-04 11:15:51 -0500

    Arrow Functions done

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -20,16 +20,43 @@ console.log(norway);
 
 
 // Function Declarations vs. Expressions
-console.log('');
+console.log('\nFunction Declaration');
 const worldPopulation = 7900;
 function percentageOfWorld1 (population){
     return population/worldPopulation*100;
 }
-const turkeyPercentage = percentageOfWorld1(88);
-const usaPercentage = percentageOfWorld1(331);
-const norwayPercentage = percentageOfWorld1(5);
+const turkeyPercentage1 = percentageOfWorld1(88);
+const usaPercentage1 = percentageOfWorld1(331);
+const norwayPercentage1 = percentageOfWorld1(5);
 
-console.log(`Turkey: ${turkeyPercentage}%`);
-console.log(`USA: ${usaPercentage}%`);
-console.log(`Norway: ${norwayPercentage}%`);
+console.log(`Turkey: ${turkeyPercentage1}%`);
+console.log(`USA: ${usaPercentage1}%`);
+console.log(`Norway: ${norwayPercentage1}%`);
+
+console.log('\nFunction Expression');
+const percentageOfWorld2 = function (population){
+    return population/worldPopulation*100;
+}
+
+const turkeyPercentage2 = percentageOfWorld2(88);
+const usaPercentage2 = percentageOfWorld2(331);
+const norwayPercentage2 = percentageOfWorld2(5);
+
+console.log(`Turkey: ${turkeyPercentage2}%`);
+console.log(`USA: ${usaPercentage2}%`);
+console.log(`Norway: ${norwayPercentage2}%`);
+
+
+// Arrow Functions
+console.log('\nArrow Function');
+const percentageOfWorld3 = population => {
+    return population/worldPopulation*100;
+}
+const turkeyPercentage3 = percentageOfWorld3(88);
+const usaPercentage3 = percentageOfWorld3(331);
+const norwayPercentage3 = percentageOfWorld3(5);
+
+console.log(`Turkey: ${turkeyPercentage3}%`);
+console.log(`USA: ${usaPercentage3}%`);
+console.log(`Norway: ${norwayPercentage3}%`);
 
