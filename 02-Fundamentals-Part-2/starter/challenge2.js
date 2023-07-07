5984f985eb4f6130ef9e85331c38e3c2939a5040
Shirin Shukurov <mr.shukurov.shirin@gmail.com>
2023-07-07 22:38:59 +0400

    Change on tips and totals done

--- 02-Fundamentals-Part-2/starter/challenge2.js
+++ 02-Fundamentals-Part-2/starter/challenge2.js
@@ -5,5 +5,5 @@ const calcTip = function(bill){
 console.log(calcTip(100));
 
 const bills = [125, 555, 44];
-const tips = [calcTip(125), calcTip(555), calcTip(44)];
-const totals = [125 + calcTip(125), 555 + calcTip(555), 44 + calcTip(44) ];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2] ];
