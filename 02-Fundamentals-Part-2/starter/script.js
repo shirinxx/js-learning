3864dafea89b9868b5964fa9ce727c90bb55e406
shirinxx <mr.shukurov.shirin@gmail.com>
2023-07-10 09:47:17 -0500

    Dot vs Brecket notation

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -34,9 +34,26 @@ console.log(yearsUntilRetirement(1999, 'Shirin'));
 const jonas = {
     firstName: 'Shirin',
     lastName: 'Shukurov',
-    age: 24,
+    birthYear: 1999,
     job: 'engineer',
     friends: ['Joni', 'Merkel', 'Hans'],
+    hasDriverLicense: true,
+
+
+    calcAge: function (){
+        this.age = 2023 - this.birthYear;
+        return this.age;
+    },
+
+    getSummary: function (){
+        return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${this.hasDriverLicense ? 'a' : 'no'} driver's license.`;
+    }
+
+
 };
 
 console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}.`);
+
+console.log(`${jonas.firstName} is ${jonas.calcAge()} year old.`);
+
+console.log(jonas.getSummary());
