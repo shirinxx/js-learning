a7c067c698b95221e569b70a28afa82b0cb9b110
shirinxx <mr.shukurov.shirin@gmail.com>
2023-07-13 07:47:37 -0500

    update

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Stop guessing u idiot';
+
+document.querySelector('.guess').value = 5;
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+  const guess = document.querySelector('.guess').value;
+});
