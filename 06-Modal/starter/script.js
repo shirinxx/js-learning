844c0533d308d697fa9d153ff04dcdeb31408f22
Shirin Shukurov <mr.shukurov.shirin@gmail.com>
2023-07-16 17:00:49 +0400

    Modal done

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+
+const modal = document.querySelector('.modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+const closeModal = function (){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+const openModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');    
+}
+
+const escClose = function(e){
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+        closeModal();
+    }
+}
+
+for (let i = 0; i < btnOpenModal.length; i++){
+    btnOpenModal[i].addEventListener('click', openModal);
+}
+
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', escClose);
