c27d9c8596e527335095b44d654860b7c352c417
shirinxx <mr.shukurov.shirin@gmail.com>
2023-07-24 10:33:16 -0500

    Scoping done

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function calcAge(birthYear) {
+    const age = 2023 - birthYear;
+    console.log(firstName);
+    let lastName = 'Shukurov';
+
+    function printAge() {
+        console.log(`${firstName} is ${age} year old.`);
+
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            const output = `Congrats ${firstName} you are millenial`;
+            var millenial = true;
+            console.log(output);
+            const lastName = 'Shukur';
+        }
+        console.log(millenial);
+    }
+    printAge();
+    console.log(lastName);
+
+    return age;
+}
+
+const firstName = 'Shirin';
+calcAge(1995);
