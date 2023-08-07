f5fe3fb1ba91e267d6d1efe2c21745afbd7dfb79
shirinxx <mr.shukurov.shirin@gmail.com>
2023-08-07 19:06:42 +0400

    Challenge 1 done

--- 09-Data-Structures-Operators/starter/challenge1.js
+++ 09-Data-Structures-Operators/starter/challenge1.js
@@ -1,45 +1,43 @@
-const game = { 
-    team1: 'Bayern Munich', 
-    team2: 'Borrussia Dortmund', 
-    players: [ 
-      [ 
-        'Neuer', 
-        'Pavard', 
-        'Martinez', 
-        'Alaba', 
-        'Davies', 
-        'Kimmich', 
-        'Goretzka', 
-        'Coman', 
-        'Muller', 
-        'Gnarby', 
-        'Lewandowski', 
-      ], 
-      [ 
-        'Burki', 
-        'Schulz', 
-        'Hummels', 
-        'Akanji', 
-        'Hakimi', 
-        'Weigl', 
-        'Witsel', 
-        'Hazard', 
-        'Brandt', 
-        'Sancho', 
-        'Gotze', 
-      ], 
-    ], 
-    score: '4:0', 
-    scored: ['Lewandowski', 'Gnarby', 'Lewandowski',  
-    'Hummels'], 
-    date: 'Nov 9th, 2037', 
-    odds: { 
-      team1: 1.33, 
-      x: 3.25, 
-      team2: 6.5, 
-    }, 
-  };
-
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
+};
 
 const [players1, players2] = game.players;
 console.log(players1, players2);
@@ -47,3 +45,23 @@ console.log(players1, players2);
 const [gk, ...fieldPlayers] = players1;
 console.log(gk, fieldPlayers);
 
+const allPlayers = [...game.players[0], ...game.players[1]];
+console.log(allPlayers);
+
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+const { team1, x: draw, team2 } = { ...game.odds };
+console.log(team1);
+console.log(team2);
+console.log(draw);
+
+const printGoals = function (...playersName) {
+    console.log(playersName);
+    console.log(`${playersName.length} goals scored in this game.`);
+};
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+
+team1 < team2 && console.log('Team1 more likely to won');
+team1 > team2 && console.log('Team2  more likely to won');
