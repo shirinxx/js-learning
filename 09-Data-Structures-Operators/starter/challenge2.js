5bb0f4c1b705e6a13844fa41bc1ae609bc350b1b
Shirin Shukurov <mr.shukurov.shirin@gmail.com>
2023-08-09 01:16:21 +0400

    challenge2 started

---
+++ 09-Data-Structures-Operators/starter/challenge2.js
@@ -0,0 +1,53 @@
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
+};
+
+// #1
+for(const [i, player] of game.scored.entries()){
+    console.log(`Goal ${i+1}: ${player}`);
+}
+
+// #2
+let sum = 0;
+for (const iterator of Object.values(game.odds)) {
+    console.log(iterator);
+    sum += iterator;
+} 
+console.log(sum/3);
