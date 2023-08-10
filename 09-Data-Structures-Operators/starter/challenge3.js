0316b6b36256073c9a90dba88c05cd0559813442
Shirin Shukurov <mr.shukurov.shirin@gmail.com>
2023-08-11 00:52:40 +0400

    Challenge3 done

---
+++ 09-Data-Structures-Operators/starter/challenge3.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const gameEvents = new Map([
+    [17, '⚽ GOAL'],
+    [36, '🔁 Substitution'],
+    [47, '⚽ GOAL'],
+    [61, '🔁 Substitution'],
+    [64, '🔶 Yellow card'],
+    [69, '🔴 Red card'],
+    [70, '🔁 Substitution'],
+    [72, '🔁 Substitution'],
+    [76, '⚽ GOAL'],
+    [80, '⚽ GOAL'],
+    [92, '🔶 Yellow card'],
+    ]);
+
+// #1
+const events = new Set(gameEvents.values());
+console.log(events);
+
+// #2
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// #3
+console.log(`An event happened, on average, every ${90 / gameEvents.size} minutes`);
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(`An event happened, on average, every ${time / gameEvents.size} minutes`);
+
+//  #4
+for (const [key, value] of gameEvents) {
+    console.log(`[${key<45? 'FIRST':'SECOND'} HALF]${key}: ${value}`);
+}
