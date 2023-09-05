ffc85a57483063196c96f29484d50d77254ee99f
shirinxx <mr.shukurov.shirin@gmail.com>
2023-09-05 17:07:00 +0400

    Array methods

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,34 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// SLICE (NOT MODIFY)
+console.log(arr.slice(2));
+console.log(arr.slice(1, 3));
+console.log(arr.slice(-1));
+console.log(arr.slice(-4));
+console.log(arr.slice(0, -1));
+console.log(arr.slice()); //SHALLOW COPY
+console.log([...arr]);
+
+//SPLICE (MODIFY)
+// console.log(arr.splice(3));
+console.log(arr);
+console.log(arr.splice(2, 1));
+console.log(arr);
+
+//REVERSE (MODIFY)
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+//CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+//JOIN
+console.log(letters.join(' - '));
