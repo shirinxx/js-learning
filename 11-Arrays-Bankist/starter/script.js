186e053a28d3451ac16ab6484d8f91fba4f40565
shirinxx <mr.shukurov.shirin@gmail.com>
2023-09-06 18:15:55 +0400

    ForEach done

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -6,31 +6,31 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
+    owner: 'Jonas Schmedtmann',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2, // %
+    pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
+    owner: 'Jessica Davis',
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
+    owner: 'Steven Thomas Williams',
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
+    owner: 'Sarah Smith',
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -66,42 +66,73 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // LECTURES
 
 const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
+    ['USD', 'United States dollar'],
+    ['EUR', 'Euro'],
+    ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
+// let arr = ['a', 'b', 'c', 'd', 'e'];
 
-// SLICE (NOT MODIFY)
-console.log(arr.slice(2));
-console.log(arr.slice(1, 3));
-console.log(arr.slice(-1));
-console.log(arr.slice(-4));
-console.log(arr.slice(0, -1));
-console.log(arr.slice()); //SHALLOW COPY
-console.log([...arr]);
+// // SLICE (NOT MODIFY)
+// console.log(arr.slice(2));
+// console.log(arr.slice(1, 3));
+// console.log(arr.slice(-1));
+// console.log(arr.slice(-4));
+// console.log('test' + arr.slice(0, -1));
+// console.log(arr.slice()); //SHALLOW COPY
+// console.log([...arr]);
 
-//SPLICE (MODIFY)
-// console.log(arr.splice(3));
-console.log(arr);
-console.log(arr.splice(2, 1));
-console.log(arr);
+// //SPLICE (MODIFY)
+// // console.log(arr.splice(3));
+// console.log(arr);
+// console.log(arr.splice(2, 1));
+// console.log(arr);
 
-//REVERSE (MODIFY)
-arr = ['a', 'b', 'c', 'd', 'e'];
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse());
-console.log(arr2);
+// //REVERSE (MODIFY)
+// arr = ['a', 'b', 'c', 'd', 'e'];
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse());
+// console.log(arr2);
 
-//CONCAT
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log([...arr, ...arr2]);
+// //CONCAT
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// console.log([...arr, ...arr2]);
 
-//JOIN
-console.log(letters.join(' - '));
+// //JOIN
+// console.log(letters.join(' - '));
+
+// //AT METHOD
+// const arr3 = [15, 33, 96];
+// console.log(arr3[0]);
+// console.log(arr3[arr3.length - 1]);
+// console.log(arr3.slice(-1));
+
+// console.log(arr3.at(-1));
+
+// console.log('shirin'.at(-1));
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const mov of movements) {
+for (const [i, mov] of movements.entries()) {
+    if (mov > 0) {
+        console.log(`Movement ${i + 1}: You deposited ${mov}`);
+    } else {
+        console.log(`Movement ${i + 1}: You withdraw ${Math.abs(mov)}`);
+    }
+}
+
+console.log(`----FOR EACH METHOD----`);
+
+movements.forEach((mov, i, arr) => {
+    if (mov > 0) {
+        console.log(`Movement ${i + 1}: You deposited ${mov}`);
+    } else {
+        console.log(`Movement ${i + 1}: You withdraw ${Math.abs(mov)}`);
+    }
+});
